22517ca61ba3f86299ae386940b9def39191b1f7
HoangIT754 <133497646+HoangIT754@users.noreply.github.com>
2024-02-21 11:43:05 +0700

    Test with Jest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState("0");
+
+  const numberHandle = () => {
+    setNumber(number === "0" ? "1" : "0");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className={number}>Hello ReactJS!</h2>
+      <h3>{number}</h3>
+      <button onClick={numberHandle}>Change Number</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test with Jest", () => {
+  test("Renders header text", () => {
+    render(<App />);
+    const Text = screen.getByText("Hello ReactJS!");
+    expect(Text).toBeInTheDocument();
+  });
+
+  test("Render button text", () => {
+    render(<App />);
+    const buttonText = screen.getByText("Change Number");
+    expect(buttonText).toBeInTheDocument();
+  });
+
+  test("Test event click Change Number button", () => {
+    render(<App />);
+    const changeNumberButton = screen.getByRole("button", {
+      name: "Change Number",
+    });
+    fireEvent.click(changeNumberButton);
+    expect(screen.getByText("1")).toBeInTheDocument();
+    fireEvent.click(changeNumberButton);
+    expect(screen.getByText("0")).toBeInTheDocument();
+  });
 });
